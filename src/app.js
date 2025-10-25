534967b7dbcc74ec09279c1df4f258c7fab05c60
GovindSingh <govindsingh988877@gmail.com>
2025-10-25 13:35:09 +0530

    age logic modification

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ import profileRoutes from "./routes/profile.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import requestRoutes from "./routes/request.routes.js";
 import dashboardRoutes from "./routes/dashboard.routes.js";
-import adminRoutes from "./routes/dashboard.routes.js";
-
+import adminRoutes from "./routes/admin.routes.js";
 
 dotenv.config();
 const app = express();
@@ -30,8 +29,59 @@ app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/request", requestRoutes);
-app.use('/api/dashboard', dashboardRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/admin", adminRoutes);
 
-
 export default app;
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import authRoutes from "./routes/auth.route.js";
+// import profileRoutes from "./routes/profile.routes.js";
+// import messageRoutes from "./routes/message.routes.js";
+// import requestRoutes from "./routes/request.routes.js";
+// import dashboardRoutes from "./routes/dashboard.routes.js";
+// import adminRoutes from "./routes/admin.routes.js";
+
+// dotenv.config();
+// const app = express();
+
+// // app.use(
+// //   cors({
+// //     origin: "https://rsaristomatch.com", // ✅ no https for local frontend
+// //     methods: ["GET", "POST", "PUT", "DELETE"],
+// //     credentials: true,
+// //   })
+// // );
+// const allowedOrigins =
+//   process.env.NODE_ENV === "production"
+//     ? ["https://rsaristomatch.com"]
+//     : [
+//         "https://rsaristomatch.com",
+//         "http://localhost:5173",
+//         "http://localhost:3000",
+//       ];
+
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Matrimonial API is running...");
+// });
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/profile", profileRoutes);
+// app.use("/api/messages", messageRoutes);
+// app.use("/api/request", requestRoutes);
+// app.use("/api/dashboard", dashboardRoutes);
+// app.use("/api/admin", adminRoutes);
+
+// export default app;
