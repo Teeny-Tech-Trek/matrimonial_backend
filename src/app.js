2fa36dc8eab011a1eeabc6094f066daacc49a683
dhruvmishra482 <dhruvmishra3829@gmail.com>
2026-02-01 13:22:58 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -126,7 +126,7 @@ app.use(
 );
 
 // IMPORTANT
-app.options("*", cors());
+app.options("/*", cors());
 
 // Increase payload size limit to handle large data like image uploads
 app.use(express.json({ limit: '50mb' }));
